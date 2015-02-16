6b63c8c150e499ef3c9825ef6fce611ec0ead643
Aleksey Fedorov <vsnrain@gmail.com>
2015-02-16 13:55:08 +0200

    Initial Commit

---
+++ NNLib/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  NNLib
+//
+//  Created by vsnRain on 16/02/2015.
+//  Copyright (c) 2015 vsnrain. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
